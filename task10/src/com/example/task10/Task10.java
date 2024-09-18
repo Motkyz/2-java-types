2b69d84dde5187b7abe625301f67d0514302bb03
MotKyz <kuznetsov.matvey.2005@mail.ru>
2024-09-19 03:33:24 +0500

    Task10 refac

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -4,9 +4,6 @@ public class Task10 {
 
     public static boolean compare(float a, float b, int precision) {
 
-        // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
-        // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
-        // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
         double permErr = 1 / Math.pow(10, precision);
         if (a!=a && b!=b) {return true;}
         if (Float.isInfinite(a) && Float.isInfinite(b) && !Float.isNaN(a + b)) {return true;}
