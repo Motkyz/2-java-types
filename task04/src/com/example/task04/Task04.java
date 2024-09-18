3a03f44f4431fb3be88079ebde68eafa208dd243
MotKyz <kuznetsov.matvey.2005@mail.ru>
2024-09-19 00:12:40 +0500

    Task04

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -4,18 +4,27 @@ public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
+        float c = (float) a;
+        switch (operation) {
+            case "+":
+                return c + b;
+            case "-":
+                return c - b;
+            case "*":
+                return c * b;
+            case "/":
+                return c / b;
+        }
         return 0;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         float result = calculate(-25, 5, "/");
         System.out.println(result);
-        */
+
     }
 
 }
